099050bd518eee921b7b10afd5555b41ac344496
Hyungjune Kwak <kwakdent95@gmail.com>
2022-01-07 16:48:03 -0800

    Basic like button added_2201071648

--- src/component/photoContent/index.tsx
+++ src/component/photoContent/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card, CardMedia, CardContent, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Card, CardMedia, CardContent, Typography, Button } from '@mui/material';
 
 type PhotoContentProps = {
     image: string;
@@ -9,23 +9,30 @@ type PhotoContentProps = {
 }
 
 const PhotoComponent = (props: PhotoContentProps) => {
+    const [like, setLike] = useState(0);
+
+    function handleLike() {
+        setLike((like+1));
+    }
+
     return (
         <Card sx={{ width: 400, height: 300, marginBottom: 5 }}>
-        <CardMedia
-            component="img"
-            height="194"
-            image={props.image}
-            alt="space-photo"
-        />
-        <CardContent>
-            <Typography variant="body1" color="text.secondary">
-                {props.roverName} rover - {props.cameraName}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-                {props.earthDate}
-            </Typography>
-        </CardContent>
-    </Card>
+            <CardMedia
+                component="img"
+                height="194"
+                image={props.image}
+                alt="space-photo"
+            />
+            <CardContent>
+                <Typography variant="body1" color="text.secondary">
+                    {props.roverName} rover - {props.cameraName}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {props.earthDate}
+                </Typography>
+            </CardContent>
+            <Button variant="contained" onClick={handleLike}>Like {like}</Button>
+        </Card>
     )
 }
 
